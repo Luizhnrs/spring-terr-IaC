ecb199a738952e9392b3059aae5a79fffb7a1fcb
Luizhnrs <Luizhnrsg@gmail.com>
2025-10-21 21:33:30 -0300

    Api Init

---
+++ api-to-deploy/src/test/java/com/lab/api_to_deploy/ApiToDeployApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lab.api_to_deploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiToDeployApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
